4aee235cde0006736e16b31af9224784cdf700d9
Guillaume <guillaumebaillieux88@gmail.com>
2024-09-05 09:44:13 +0200

    Article.php

--- migrations/Version20240905071902.php
+++ migrations/Version20240905071902.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240903142847 extends AbstractMigration
+final class Version20240905071902 extends AbstractMigration
 {
     public function getDescription(): string
     {
